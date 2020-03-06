31638b07a7001530fc46cafcebedc30f1071d7ab
Zheng Yu Koo <zheng@Admins-MBP.localdomain>
2020-03-06 12:39:56 +0800

    Add knex, init knex, create teacher migration file

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      database: "develop",
+      user: "develop",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  staging: {
+    client: "mysql",
+    connection: {
+      database: "staging",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "mysql",
+    connection: {
+      database: "production",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
