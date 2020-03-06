9fc4c3673cf2ed634ebd37b6967d0e2696bdbca2
Zheng Yu Koo <zheng@Admins-MBP.localdomain>
2020-03-06 17:52:09 +0800

    Add functionality for GET /api/commonstudents controller

--- src/controllers/common-students.js
+++ src/controllers/common-students.js
@@ -1,5 +1,27 @@
-const commonStudents = (req, res, next) => {
-  res.json({ message: "commonStudents works" });
+const db = require("../db");
+
+const commonStudents = async (req, res, next) => {
+  try {
+    const teachers = await db("teacher").whereIn("email", req.query.teacher);
+    const teacherIds = teachers.map(teacher => teacher.id);
+
+    const students = await db
+      .select("cs.student_id", "student.email")
+      .from(
+        db
+          .select("student_id")
+          .from("student_teacher")
+          .whereIn("teacher_id", teacherIds)
+          .groupBy("student_id")
+          .havingRaw("count(distinct teacher_id) = " + teacherIds.length)
+          .as("cs")
+      )
+      .innerJoin("student", "cs.student_id", "id");
+
+    res.json({ students });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = commonStudents;
