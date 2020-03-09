527cdfb03e78f646240db6295de887055e7f8a11
Zheng Yu Koo <zheng@Admins-MBP.localdomain>
2020-03-09 18:17:35 +0800

    Update middlewares/error-handler

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -2,12 +2,13 @@ const { ServerError } = require("../types");
 const { handleServerError } = require("../utils");
 
 const errorHandler = (err, req, res, next) => {
-  // TODO: replace this with a proper logging library
-  console.error(err);
-
   if (err instanceof ServerError) {
+    // TODO: replace this with a proper logging library
+    console.error(err.code, err.message); // only print the error code and error message
     handleServerError(err, res);
   } else {
+    // TODO: replace this with a proper logging library
+    console.error(err); // print everything incl. stack trace
     return res.status(500).json({ message: err.message });
   }
 };
