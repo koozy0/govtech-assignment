f379c234162354089746390f59920b975b5211f2
Zheng Yu Koo <zheng@Admins-MBP.localdomain>
2020-03-09 18:13:30 +0800

    Update middlewares/logger with TODO item

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,3 +1,5 @@
+// TODO: replace this with a proper logging library like morgan + winston to avoid performance problems
+// basic logger using console.log
 const logger = (req, res, next) => {
   console.log(`[${new Date()}] ${req.method} ${req.url}`);
   next();
